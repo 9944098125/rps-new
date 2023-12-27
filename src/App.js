4f683c81709091b660f25a73e5ba1c28a0d627ac
Srinivas Akella <srinivas72075@gmail.com>
2023-12-27 16:45:33 +0530

    added downloaded images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import Game from "./Game";
 import "./App.css";
+import rock from "./rock.png";
+import scissors from "./scissors.webp";
+import paper from "./paper.png";
 
 const choicesList = [
-  {
-    id: "ROCK",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Fist.svg/1122px-Fist.svg.png",
-  },
-  {
-    id: "SCISSORS",
-    image: "https://www.rock-paper-scissors-game.com/i/scissors.png",
-  },
-  {
-    id: "PAPER",
-    image:
-      "https://cdn.pixabay.com/photo/2014/04/02/16/16/hand-306763_1280.png",
-  },
+	{
+		id: "ROCK",
+		image: rock,
+	},
+	{
+		id: "SCISSORS",
+		image: scissors,
+	},
+	{
+		id: "PAPER",
+		image: paper,
+	},
 ];
 
 function App() {
-  return <Game choicesList={choicesList} />;
+	return <Game choicesList={choicesList} />;
 }
 
 export default App;
